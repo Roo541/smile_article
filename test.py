6d5c1855984f46484ec5d4e8618789f55c270035
Roo Fernando <lahiru.fernando54@gmail.com>
2022-04-04 18:18:07 -0700

    commit work from intel

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 import pandas as pd  
+import numpy as np 
+import math 
 
-a = 5
+def call(s, k, r, t, vol):
+    call_price = s*(N())
+
+    call_price = 0.0
+    return call_price
+
+
+
+s = 10.0
+k = 12.0
+r = 0.01
+t = 30/365.0
+vol = .25
